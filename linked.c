b93de4dcec34b14cabbc95b0ff5a35ca0ebace72
Abhijeet Vetal <120811664+abhijeet805@users.noreply.github.com>
2024-02-09 12:38:28 +0530

    Add files via upload

---
+++ linked.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main(){
+   int f[50],p,i,st,len,j,c,k,a;
+   for(i=0;i<50;i++)
+     f[i]=0;
+   printf("Enter how many block already allocated");
+   scanf("%d",&p);
+   printf("Enter The Blocks already allocated");
+   for(i=0;i<p;i++){
+    scanf("%d",&a);
+    f[a]=1;
+   }
+   x:printf("Enter index staring and length:");
+   scanf("%d%d",&st,&len);
+   k=len;
+   if(f[st]==0){
+    for(j=st;j<(st+k);j++){
+      if(f[j]==0){
+       f[j]=1;
+       printf("%d-----------> %d \n",j,f[j]);
+      }
+      else{
+        printf("%d Block are already Allocated \n",j);
+        k++;
+      }
+    }
+   }
+   
+   else
+    printf("%d Starting block is already allocated \n",st);
+    printf("Do you want to enter more file(Yes-1 /No-0)");
+    scanf("%d",&c);
+    if(c==1)
+      goto x;
+    else
+      exit(0);
+}
